3bfb6c2a3902e09ad7e81b194fd60644731828ac
Florian Ulbricht <f.ulbricht@campus.tu-berlin.de>
2025-03-25 09:34:10 +0100

    now any key for exit

--- Python/chromatic_scale_shuffler.py
+++ Python/chromatic_scale_shuffler.py
@@ -4,6 +4,7 @@ Chromatic scale shuffler for random chromatic guitar exercise.
 
 import random
 import numpy as np
+import keyboard
 
 # variables
 numbers = [1, 2, 3, 4]
@@ -22,8 +23,8 @@ print("Finger set:", numbers)
 print(f"Start with: {strokes[0]}")
 print("Index finger on fret:", int(index_finger_fret[0]))
 
-# keep window open until input
-input("\nPress ENTER to close")
+print("Press any key to exit...")
+keyboard.read_event()  # Wait for any key before closing
 
 # build .exe with:
 # pyinstaller --onefile chromatic_scale_shuffler.py
